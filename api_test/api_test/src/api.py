0f5d36eb7fdf7b8926dc4d1089c15affc424472c
v-song <viviensong2011@gmail.com>
2022-10-24 14:38:49 -0700

    -m finish api_test

--- api_test/api_test/src/api.py
+++ api_test/api_test/src/api.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from src.test import Adder
 
 app = FastAPI(title ="new project")
 
 
 @app.get("/")
 def home():
+    adder = Adder()
     resources = {
-        "Title": "Hello 123"
+        "Title": "Hello 123",
+        "Count": adder.count
     }
     return resources
---
+++ api_test/api_test/src/test.py
@@ -0,0 +1,8 @@
+class Adder:
+    count= 0
+
+    def __init__(self):
+        Adder.count += 1
+
+    def add_my_stuff(self, a, b):
+        return a+b
